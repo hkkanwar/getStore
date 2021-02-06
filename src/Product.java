10763fd744f20b6bf758bb0d6e459a02e8e9c309
koookee <55768904+koookee@users.noreply.github.com>
2021-02-06 05:20:47 -0500

    Created the Product class

---
+++ src/Product.java
@@ -0,0 +1,21 @@
+public class Product {
+    private String name;
+    private int ID;
+    private double price;
+
+    public Product(String name, int ID, double price){
+        this.name = name;
+        this.ID = ID;
+        this.price = price;
+    }
+
+    public String getName() { return name; }
+
+    public int getID() {
+        return ID;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
