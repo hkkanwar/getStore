d2ae5a8fce0dd76d3b0d9c1770c71f36d13855e1
koookee <55768904+koookee@users.noreply.github.com>
2021-03-18 18:19:55 -0400

    Created the store test package with Junit5 . Added the methods of StoreManager to StoreManagerTest (Did not implement the test methods yet)

---
+++ src/storetest/StoreManagerTest.java
@@ -0,0 +1,63 @@
+package storetest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StoreManagerTest {
+
+    @BeforeEach
+    public void init() {
+        // test fixture; executed before each test
+        // creation of required objects, program state, etc.
+    }
+
+    @Test
+    public void testCheckStock() {
+
+    }
+
+    @Test
+    public void testProcessTransaction() {
+
+    }
+
+    @Test
+    public void testAddItemToCart() {
+
+    }
+
+    @Test
+    public void testRemoveItemFromCart() {
+
+    }
+
+    @Test
+    public void testGetCart() {
+
+    }
+
+    @Test
+    public void testAssignNewCartID() {
+
+    }
+
+    @Test
+    public void testShowInventory() {
+
+    }
+
+    @Test
+    public void testShowCart() {
+
+    }
+
+    @Test
+    public void testReturnItemsToInventory() {
+
+    }
+}
