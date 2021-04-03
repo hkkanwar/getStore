6232ee3b1cdd62cd53e7ad99697643da2c571bb9
koookee <55768904+koookee@users.noreply.github.com>
2021-04-03 10:23:50 -0400

    Added an image attribute to product. buttonAdd now adds products to cart. Only issue is that the stock quantity doesn't update in the GUI

--- src/StoreClass/Product.java
+++ src/StoreClass/Product.java
@@ -5,45 +5,57 @@ package StoreClass;
  * @version 1.0
  */
 
+import javax.swing.*;
 import java.util.Objects;
 
 public class Product {
     private final String name;
     private final int id;
     private final double price;
+    private final ImageIcon image;
 
     /**
      * Product Constructor
-     * @param name
-     * @param id
-     * @param price
+     * @param name the String name of the product
+     * @param id the int id of the product
+     * @param price the double price of the product
+     * @param image an ImageIcon of the product image
      */
-    public Product(String name, int id, double price){
+    public Product(String name, int id, double price, ImageIcon image){
         this.name = name;
         this.id = id;
         this.price = price;
+        this.image = image;
     }
 
     /** Get the name of product
-     * @return name
+     * @return the String name
      */
     public String getName() { return name; }
 
     /** Get the id of product
-     * @return id
+     * @return the int id
      */
     public int getId() { return id; }
 
     /** Get the price of product
-     * @return price
+     * @return the double price
      */
     public double getPrice() { return price; }
 
+    /**
+     * Get the image of the product
+     * @return the ImageIcon image
+     */
+    public ImageIcon getImageIcon(){
+        return image;
+    }
     /**
      * Compares two Product objects and checks if they're equal
      * @param o the Object to compare with
      * @return true if they have the same id, price, and name. false otherwise
      */
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/StoreClass/StoreVCart.java
+++ src/StoreClass/StoreVCart.java
@@ -32,31 +32,39 @@ public class StoreVCart {
         }
 
         //We should probably pass the object instead of all these parameters
-        private JPanel productCard(String name, double price, int stock, ImageIcon image){
+        private JPanel productCard(Product product, int stock){
+            JPanel borderLayout = new JPanel(new BorderLayout());
+            JPanel gridLayout = new JPanel(new GridLayout(1,2));
             JPanel description = new JPanel(new BorderLayout());
             JButton buttonAdd = new JButton("+");
             JButton buttonSub = new JButton("-");
-            JLabel newImage = new JLabel(image); // This is so that it doesn't use/move the passed image in the argument
-            JLabel nameLabel = new JLabel(name);
-            JLabel priceLabel = new JLabel("Price: " + Double.toString(price) + "$");
-            JLabel stockLabel = new JLabel("Stock: " + Integer.toString(stock));
+            JLabel imageLable = new JLabel(product.getImageIcon());
+            JLabel nameLabel = new JLabel(product.getName());
+            JLabel priceLabel = new JLabel("Price: " + Double.toString(product.getPrice()) + "$");
+            JLabel stockLabel = new JLabel("Stock: " + Integer.toString(storeManager.checkStock(product)));
+            buttonAdd.addActionListener(new ActionListener() {
+                // this method will be called when we click the button
+                @Override
+                public void actionPerformed(ActionEvent ae) {
+                    if(storeManager.checkStock(product) > 0) {
+                        storeManager.addItemToCart(product, 1, cartID);
+                    }
+                    if (storeManager.checkStock(product) == 0) buttonAdd.setEnabled(false);
+            }});
             description.add(nameLabel, BorderLayout.NORTH);
             description.add(priceLabel, BorderLayout.CENTER);
             description.add(stockLabel, BorderLayout.SOUTH);
-            JPanel borderLayout = new JPanel(new BorderLayout());
-            borderLayout.add(newImage, BorderLayout.CENTER);
-            borderLayout.add(description, BorderLayout.NORTH);
-            JPanel gridLayout = new JPanel(new GridLayout(1,2));
             gridLayout.add(buttonSub);
             gridLayout.add(buttonAdd);
+            borderLayout.add(imageLable, BorderLayout.CENTER);
+            borderLayout.add(description, BorderLayout.NORTH);
             borderLayout.add(gridLayout, BorderLayout.PAGE_END);
             borderLayout.setBorder(BorderFactory.createLineBorder(Color.black));
             return borderLayout;
         }
 
         public void displayGUI(){
-            // some things you should specify when creating your JFrame
-            frame.setTitle("TEST FOR CART");
+            frame.setTitle("Client StoreView");
 
             // create JLabels
             JLabel headerLabel = new JLabel("Welcome to our Store");
@@ -70,18 +78,8 @@ public class StoreVCart {
             JPanel cartP = new JPanel(new BorderLayout());
             JPanel cartItemsGrid = new JPanel(new GridLayout(3,1));
 
-            //icons
-            ImageIcon applesImage = new ImageIcon(new ImageIcon("src/StoreClass/Apples.jpg").getImage().getScaledInstance(90, 90, Image.SCALE_DEFAULT));
-            ImageIcon orangesImage = new ImageIcon(new ImageIcon("src/StoreClass/Oranges.jpg").getImage().getScaledInstance(90, 90, Image.SCALE_DEFAULT));
-            ImageIcon lemonsImage = new ImageIcon(new ImageIcon("src/StoreClass/Lemons.jpg").getImage().getScaledInstance(90, 90, Image.SCALE_DEFAULT));
             ImageIcon cartImage = new ImageIcon(new ImageIcon("src/StoreClass/cart1.png").getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT));
-            //_____________________________________________
-            JLabel appleImageLabel = new JLabel(applesImage);
-            JLabel orangesImageLabel = new JLabel(orangesImage);
-            JLabel lemonsImageLabel = new JLabel(lemonsImage);
-            JLabel cartText = new JLabel("Cart");
-            //cartText.setMinimumSize(new Dimension(30, 30));
-            //cartP.setBackground(Color.pink);
+
             JButton cartB = new JButton(cartImage);
             JButton checkoutB = new JButton("Checkout");
             JButton quitB = new JButton("Quit");
@@ -101,103 +99,46 @@ public class StoreVCart {
                         showCartText.setVisible(false);
                     }
                 }});
-            //cartB.setMinimumSize(new Dimension(10, 10));
-
-
             bodyPanel.add(cartP, BorderLayout.WEST);
             bodyPanel.add(inventoryP, BorderLayout.CENTER);
             bodyPanel.add(footerPanel, BorderLayout.SOUTH);
             footerPanel.add(showCartText, BorderLayout.WEST);
-            //footerPanel.add(quitB, BorderLayout.CENTER);
             cartP.add(cartB, BorderLayout.NORTH);
             cartP.add(checkoutB, BorderLayout.SOUTH);
             JPanel productCardsGrid = new JPanel(new GridLayout(2,2));
 
             cartItemsGrid.setPreferredSize(new Dimension(10, 30));
             cartP.add(cartItemsGrid, BorderLayout.CENTER);
+            for (Product p : storeManager.showInventory()){
+                productCardsGrid.add(productCard(p, storeManager.checkStock(p)));
+            }
+            /*
             productCardsGrid.add(productCard("Apples", 1.3, 10, applesImage));
             productCardsGrid.add(productCard("Oranges", 3.2, 4, orangesImage));
             productCardsGrid.add(productCard("Lemons", 0.6, 34, lemonsImage));
 
             cartItemsGrid.add(productCard("Apples", 1.3, 10, applesImage));
-            //cartItemsGrid.add(productCard("Oranges", 3.2, 4, orangesImage));
-            //cartItemsGrid.add(productCard("Lemons", 0.6, 34, lemonsImage));
-
-            inventoryP.add(productCardsGrid, BorderLayout.CENTER);
-            JLabel inventoryText = new JLabel("Inventory");
-            JButton button = new JButton("Testing");
-            JButton button1 = new JButton("Testing1");
-            JButton button2 = new JButton("Testing2");
-            JButton button3 = new JButton("Testing3");
-            JButton buttonAdd = new JButton("+");
-            JButton buttonSub = new JButton("-");
-
-            inventoryP.add(inventoryText, BorderLayout.NORTH);
-            inventoryP.setBorder(BorderFactory.createLineBorder(Color.black));
-            /*
-            GridBagConstraints constraints = new GridBagConstraints();
-            constraints.gridx = 0;
-            constraints.gridy = 0;
-            constraints.insets = new Insets(0,20,15,20);
-            //bodyPanel.add(cartText,constraints);
-            //bodyPanel.add(cartB,constraints);
-            //constraints.gridx = 1;
-            //constraints.gridy = 0;
-
-            bodyPanel.add(cartP,constraints);
-            constraints.gridx = 0;
-            constraints.gridy = 0;
-            bodyPanel.add(inventoryText,constraints);
-            constraints.gridx = 1;
-            constraints.gridy = 0;
-            bodyPanel.add(appleImageLabel,constraints);
-            constraints.gridx = 2;
-            constraints.gridy = 1;
-            bodyPanel.add(button, constraints);
-            constraints.gridx = 3;
-            constraints.gridy = 1;
-            bodyPanel.add(button1, constraints);
-            constraints.gridx = 1;
-            constraints.gridy = 2;
-            bodyPanel.add(button2, constraints);
-            constraints.gridx = 1;
-            constraints.gridy = 3;
-            constraints.weighty = 1.0;
-
-            constraints.insets = new Insets(40,0,0,0);
-            JPanel card1 = new JPanel(new BorderLayout());
-            card1.add(appleImageLabel, BorderLayout.CENTER);
-            card1.add(applesText, BorderLayout.PAGE_START);
-            JPanel gridLayout = new JPanel(new GridLayout(1,2));
-            gridLayout.add(buttonAdd);
-            gridLayout.add(buttonSub);
-            card1.add(gridLayout, BorderLayout.PAGE_END);
-            JPanel card2 = productCard("Apples", 12.2, 12, appleImageLabel);
-            bodyPanel.add(card2, constraints);
-            bodyPanel.setBackground(Color.CYAN);
-            bodyPanel.setOpaque(true);
+            cartItemsGrid.add(productCard("Oranges", 3.2, 4, orangesImage));
+            cartItemsGrid.add(productCard("Lemons", 0.6, 34, lemonsImage));
 
              */
 
+            inventoryP.add(productCardsGrid, BorderLayout.CENTER);
+            JLabel inventoryText = new JLabel("Inventory");
 
-            // -------------------------------------------------
+            inventoryP.add(inventoryText, BorderLayout.NORTH);
+            inventoryP.setBorder(BorderFactory.createLineBorder(Color.black));
 
-            // adding JLabels to the respective JPanel
+
             headerPanel.add(headerLabel, BorderLayout.CENTER);
 
 
-            // set the preferred sizes and colours here
-            // headerPanel.setPreferredSize(new Dimension(250, 100));
-            // bodyPanel.setPreferredSize(new Dimension(250, 100));
-
-
-            // add your JLabels to the panel here
             mainPanel.add(headerPanel, BorderLayout.PAGE_START);
             mainPanel.add(bodyPanel, BorderLayout.CENTER);
-            //mainPanel.setPreferredSize(new Dimension(500, 500));
-            // pack
+
             frame.add(mainPanel);
             frame.pack();
+
             // add the window listener
             // we no longer want the frame to close immediately when we press "x"
             frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
@@ -215,7 +156,6 @@ public class StoreVCart {
             // the frame is not visible until we set it to be so
             frame.setVisible(true);
             //frame.setMinimumSize(new Dimension(300, 300));
-            System.out.println(frame.getSize()); //Default w182 & h155
         }
 
 
